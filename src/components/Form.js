83f09b1afa0a80f39053ef6aed17ffd20afc82a8
pedrodc1236@gmail.com <pedrodc1236@gmail.com>
2022-02-23 17:27:33 -0300

    requisito 1 e 2 completos

---
+++ src/components/Form.js
@@ -0,0 +1,162 @@
+import React, { Component } from 'react';
+import './Form.css';
+import Proptypes from 'prop-types';
+
+class Form extends Component {
+  render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
+    return (
+      <form>
+        <h1>Adicionar nova carta</h1>
+
+        <label
+          htmlFor="nomeCard"
+        >
+          Nome
+          <br />
+          <input
+            type="text"
+            name="nomeCard"
+            data-testid="name-input"
+            value={ cardName }
+            onChange={ onInputChange }
+          />
+        </label>
+
+        <label
+          htmlFor="textDescrition"
+        >
+          Descrição
+          <br />
+          <textarea
+            name="textDescrition"
+            data-testid="description-input"
+            value={ cardDescription }
+            onChange={ onInputChange }
+          />
+        </label>
+
+        <label
+          htmlFor="firstAtribute"
+        >
+          Attr01
+          <input
+            type="number"
+            name="firstAttribute"
+            data-testid="attr1-input"
+            value={ cardAttr1 }
+            onChange={ onInputChange }
+          />
+        </label>
+
+        <label
+          htmlFor="secondAttribute"
+        >
+          Attr02
+          <input
+            type="number"
+            name="secondAttribute"
+            data-testid="attr2-input"
+            value={ cardAttr2 }
+            onChange={ onInputChange }
+          />
+        </label>
+
+        <label
+          htmlFor="thirdAttribute"
+        >
+          Attr03
+          <input
+            type="number"
+            name="thirdAttribute"
+            data-testid="attr3-input"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
+          />
+        </label>
+
+        <label
+          htmlFor="imageCard"
+        >
+          Imagem
+          <input
+            type="text"
+            name="imageCard"
+            data-testid="image-input"
+            value={ cardImage }
+            onChange={ onInputChange }
+          />
+        </label>
+
+        <label
+          htmlFor="rarityCard"
+        >
+          Raridade
+          <br />
+          <select
+            name="rarityCard"
+            data-testid="rare-input"
+            value={ cardRare }
+            onChange={ onInputChange }
+          >
+            <option value="normal">Normal</option>
+            <option value="raro">Raro</option>
+            <option value="muito raro">Muito Raro</option>
+          </select>
+        </label>
+
+        <label
+          htmlFor="superTrunfo"
+        >
+          Super Trybe Trunfo
+          <input
+            type="checkbox"
+            name="superTrunfo"
+            data-testid="trunfo-input"
+            checked={ cardTrunfo }
+            onChange={ onInputChange }
+          />
+        </label>
+
+        <button
+          type="submit"
+          data-testid="save-button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+        >
+          Salvar
+        </button>
+      </form>
+    );
+  }
+}
+
+Form.propTypes = {
+  cardName: Proptypes.string.isRequired,
+  cardDescription: Proptypes.string.isRequired,
+  cardAttr1: Proptypes.string.isRequired,
+  cardAttr2: Proptypes.string.isRequired,
+  cardAttr3: Proptypes.string.isRequired,
+  cardImage: Proptypes.string.isRequired,
+  cardRare: Proptypes.string.isRequired,
+  cardTrunfo: Proptypes.bool.isRequired,
+  hasTrunfo: Proptypes.bool.isRequired,
+  isSaveButtonDisabled: Proptypes.bool.isRequired,
+  onInputChange: Proptypes.func.isRequired,
+  onSaveButtonClick: Proptypes.func.isRequired,
+};
+
+export default Form;
